34cee6f87aeea7904bc10c02c30009c424156c13
poo <poo@u.c>
2022-05-02 23:04:24 +1000

    typo

--- pwman/models.py
+++ pwman/models.py
@@ -5,7 +5,7 @@ from base64 import b64encode, b64decode
 class Passwords(models.Model):
     
     class Meta:
-        sorting_order = "Website"
+        ordering = "Website"
 
     site = models.CharField(max_length=1024)
     username = models.CharField(max_length=255)
